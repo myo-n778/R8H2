3571f9127458c94c7c6b306698f1a5085d5b5b09
myo-n778 <myo-n778@users.noreply.github.com>
2026-05-07 18:15:44 +0900

    Use H2 dedicated GAS web app URL

--- problemset/common/science-shared.js
+++ problemset/common/science-shared.js
@@ -29,7 +29,7 @@
   }
 
   window.ScienceShared = {
-    gasUrl: "https://script.google.com/macros/s/AKfycbzsheqTjtvzClj2RoRhlOln1hIbVNdncqnFjPaBPUOqBwyTPM7ZuFM8yINAFL5G2VlKrg/exec",
+    gasUrl: "https://script.google.com/macros/s/AKfycbyazYucFCKdi67W6OuzaKc6YTil1e6E5Rq5R6yE1u7Jd4Tdjqrubr1rzZrituOChZSJsA/exec",
     normalizeUserName: normalizeUserName,
     formatDateYmd: formatDateYmd,
     buildEraSessionStats: buildEraSessionStats
